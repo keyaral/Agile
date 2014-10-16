afc341141f6abda831f69b25147226d3ea8c0ffb
Alex Campbell <campbealex2@myvuw.ac.nz>
2014-10-17 12:08:45 +1300

    Use a simple equal-size grid, instead of nested JSplitPanes.

--- Agile/src/swen302/gui/MultiSplitPane.java
+++ Agile/src/swen302/gui/MultiSplitPane.java
@@ -1,11 +1,10 @@
 package swen302.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Container;
+import java.awt.GridLayout;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
-import javax.swing.JSplitPane;
 
 public class MultiSplitPane extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -25,23 +24,10 @@ public class MultiSplitPane extends JPanel {
 		if(contents.length == 0)
 			return;
 
-		//setBorder(BorderFactory.createLineBorder(Color.red));
+		this.setLayout(new GridLayout(1, contents.length*2-1, 5, 0));
 
-		// Make a chain of nested splitters.
-		// The right side of each splitter is either the final element, or another splitter.
-
-		Container nextParent = this;
-
-		for(int k = 0; k < contents.length - 1; k++) {
-			JSplitPane splitter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-			//splitter.setBorder(BorderFactory.createLineBorder(Color.red));
-			splitter.setBorder(null);
-			nextParent.add(splitter, (nextParent == this ? BorderLayout.CENTER : null));
-			splitter.add(contents[k]);
-			nextParent = splitter;
-		}
-
-		nextParent.add(contents[contents.length-1]);
+		for(JComponent jc : contents)
+			this.add(jc);
 
 		revalidate();
 
